2ed601380e24d255756f024886768d1e78f405d5
shane-misquith <shane.misquith@gmail.com>
2020-04-23 01:02:04 +0530

    update post_details
    added  comment model
    looped over comments

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,13 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk' : self.pk})
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default = timezone.now)
+
+    def __str__(self):
+        return f'{self.post}-{self.author}'
